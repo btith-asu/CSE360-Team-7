86262fd4850b34dbaf9fc3fb991e7202e0d6ec31
Roper13 <79863083+Roper13@users.noreply.github.com>
2022-04-03 16:15:55 -0700

    Add files via upload

--- DeliveryController.java
+++ DeliveryController.java
@@ -2,11 +2,22 @@ package com.example.cse360;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import javafx.scene.text.Text;
 
 public class DeliveryController {
+    public int timeMinutes;
+    private int finishTime;
+    private int startTime;
+    private int timeElapsed;
+    private int updatedTime;
+    private int updatedOrders;
+    private int ratio;
+    private int counter;
+
+    //Pane
 
     @FXML
     private HBox NavBar;
@@ -14,17 +25,69 @@ public class DeliveryController {
     @FXML
     private Region navCart;
 
+    //Text
+
     @FXML
     private Text ordersAhead;
 
     @FXML
     private Text timeAhead;
 
+    //Buttons
+
     @FXML
-    private void calculateTime(ActionEvent event) {
+    private Button calTime;
+
+    @FXML
+    private Button upOrderNum;
+
+    @FXML
+    public int calculateTime(ActionEvent event) {
         DeliveryDetails Orders = new DeliveryDetails();
-        Orders.getnumOfOrders();
-        ordersAhead.setText(String.valueOf(Orders.getnumOfOrders() + " Orders"));
-        timeAhead.setText(String.valueOf(Orders.gettimeArrival() + " Minutes"));
+        startTime();
+        ordersAhead.setText(String.valueOf(Orders.orderAhead + " Orders"));
+        timeAhead.setText(String.valueOf(Orders.timeArrival + " Minutes"));
+        calTime.setDisable(true);
+        upOrderNum.setDisable(false);
+        return 0;
     }
-}
+
+    private long startTime() {
+        long start = System.nanoTime();
+        startTime = (int) (start / 1000000000);
+        return startTime;
+    }
+
+    private long endTime() {
+        long finish = System.nanoTime();
+        finishTime = (int) (finish / 1000000000);
+        return finishTime;
+    }
+
+    private int calculateDifference() {
+        timeElapsed = (finishTime - startTime);
+        timeMinutes = (timeElapsed / 60);
+        return this.timeMinutes;
+    }
+
+    @FXML
+    private void updateTime(ActionEvent event) {
+        endTime();
+        calculateDifference();
+        DeliveryDetails Orders = new DeliveryDetails();
+        updatedTime = Orders.timeArrival - timeMinutes;
+        updatedOrders = Orders.orderAhead;
+        ratio = Orders.timeArrival/Orders.orderAhead;
+        if (ratio <= (Orders.timeArrival - updatedTime))
+            counter = ratio/(Orders.timeArrival - updatedTime);
+            updatedOrders = updatedOrders - (counter);
+        if (updatedOrders <= 0)
+            updatedOrders = 0;
+        if(updatedTime == 0){
+            ordersAhead.setText(" ");
+            timeAhead.setText("Your Order Has Been Delivered!");}
+        else{
+            ordersAhead.setText(String.valueOf(updatedOrders + " Orders"));
+            timeAhead.setText(String.valueOf(updatedTime)+ " Minutes");}
+        }
+    }
